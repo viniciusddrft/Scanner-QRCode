aae621e86453f697e54a9eb1af29700bf827d99c
viniciusddrft <viniciusddrft5@gmail.com>
2021-12-12 07:57:08 -0300

    change dependences

---
+++ lib/src/shared/admob/controller/admob_example.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class Admob {
+  static int _numBannerLoadAttempts = 0;
+  static const int _maxFailedLoadAttempts = 3;
+
+  static late int anchoredBannerHeightAd;
+  static late int anchoredBannerWidthAd;
+
+//------------------------------------------------------------------------------
+  static Future<BannerAd?> createAnchoredBanner(BuildContext context) async {
+    final AnchoredAdaptiveBannerAdSize? _size =
+        await AdSize.getAnchoredAdaptiveBannerAdSize(
+      Orientation.portrait,
+      MediaQuery.of(context).size.width.truncate(),
+    );
+
+    if (_size == null) {
+      debugPrint('Unable to get height of anchored banner.');
+      return null;
+    }
+
+    final BannerAd banner = BannerAd(
+      size: _size,
+      request: const AdRequest(),
+      adUnitId: BannerAd.testAdUnitId,
+      listener: BannerAdListener(
+        onAdLoaded: (Ad ad) => debugPrint('$BannerAd loaded.'),
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          debugPrint('Ad failed to load: $error');
+          _numBannerLoadAttempts++;
+
+          if (_numBannerLoadAttempts != _maxFailedLoadAttempts) {
+            createAnchoredBanner(context);
+          } else {
+            ad.dispose();
+          }
+        },
+        onAdOpened: (Ad ad) => debugPrint('$BannerAd onAdOpened.'),
+        onAdClosed: (Ad ad) => debugPrint('$BannerAd onAdClosed.'),
+        onAdImpression: (Ad ad) => debugPrint('Success|o| .'),
+      ),
+    );
+    return banner;
+  }
+
+//------------------------------------------------------------------------------
+  static BannerAd createBannerRetanguleAd() {
+    final BannerAd banner = BannerAd(
+      adUnitId: BannerAd.testAdUnitId,
+      size: AdSize.mediumRectangle,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (Ad ad) => debugPrint('Ad loaded.'),
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          debugPrint('Ad failed to load: $error');
+          _numBannerLoadAttempts++;
+          if (_numBannerLoadAttempts != _maxFailedLoadAttempts) {
+            createBannerRetanguleAd();
+          } else {
+            ad.dispose();
+          }
+        },
+        onAdOpened: (Ad ad) => debugPrint('Ad opened.'),
+        onAdClosed: (Ad ad) => debugPrint('Ad closed'),
+        onAdImpression: (Ad ad) => debugPrint('Success |O| .'),
+      ),
+    );
+    return banner;
+  }
+}
